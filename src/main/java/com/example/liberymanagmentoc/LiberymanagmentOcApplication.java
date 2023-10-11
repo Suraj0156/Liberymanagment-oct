9e086a9b1b9c87b2e659e9e50289835a0570e67c
suraj <surajhinge77@gmail.com>
2023-10-11 10:13:03 +0530

    first commit

---
+++ src/main/java/com/example/liberymanagmentoc/LiberymanagmentOcApplication.java
@@ -0,0 +1,13 @@
+package com.example.liberymanagmentoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberymanagmentOcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberymanagmentOcApplication.class, args);
+	}
+
+}
